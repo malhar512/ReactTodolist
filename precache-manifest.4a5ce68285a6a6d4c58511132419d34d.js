ac927e1bd037e5d4b1532ade9195b0c0ce0effc2
Malhar Surangalikar <malhar10000@gmail.com>
2020-07-26 14:17:56 +0530

    Updates

---
+++ precache-manifest.4a5ce68285a6a6d4c58511132419d34d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca504306d5107089845665f0cdbbbcfb",
+    "url": "/ReactTodolist/index.html"
+  },
+  {
+    "revision": "a561f7d765a63d59cb97",
+    "url": "/ReactTodolist/static/js/2.de3b742d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactTodolist/static/js/2.de3b742d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bee67249399ac407f5a0",
+    "url": "/ReactTodolist/static/js/main.c7d5a9c7.chunk.js"
+  },
+  {
+    "revision": "be57bb25705e178bd036",
+    "url": "/ReactTodolist/static/js/runtime-main.048ca6ae.js"
+  }
+]);
